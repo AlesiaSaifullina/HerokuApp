785c11db4a21b340ab3c87e4f18a5f3aed7feb7c
Alesya S <saifulina.qa@gmail.com>
2022-03-09 22:06:58 +0300

    TMS-6-added tests

---
+++ src/test/java/CheckboxesTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class CheckboxesTest extends BaseTest{
+
+    @Test
+    public void checkboxes() {
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+        boolean checkbox1 = driver.findElements(By.cssSelector("[type=checkbox]")).get(0).isSelected();
+        assertFalse(checkbox1, "checkbox 1 is selected");
+        driver.findElements(By.cssSelector("[type=checkbox]")).get(0).click();
+        checkbox1 = driver.findElements(By.cssSelector("[type=checkbox]")).get(0).isSelected();
+        assertTrue(checkbox1, "checkbox 1 is not selected");
+
+        boolean checkbox2 = driver.findElements(By.cssSelector("[type=checkbox]")).get(1).isSelected();
+        assertTrue(checkbox2, "checkbox 2 is not selected");
+        driver.findElements(By.cssSelector("[type=checkbox]")).get(1).click();
+        checkbox2 = driver.findElements(By.cssSelector("[type=checkbox]")).get(1).isSelected();
+        assertFalse(checkbox2, "checkbox 2 is selected");
+    }
+}
